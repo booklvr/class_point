c05dc3a2304ebffef1b68c2ffd50980898e043cd
Nick de Waal <nick.a.dewaal@gmail.com>
2020-05-20 10:50:09 +0900

    push to heroku

--- public/js/boysVsGirls/controller.js
+++ public/js/boysVsGirls/controller.js
@@ -28,7 +28,7 @@ var controller = (function(UI) {
         DOM.teams.addEventListener('click', UI.changePointStudent);
         DOM.teams.addEventListener('click', UI.changeTeamPoints);
         DOM.gameContainer.addEventListener('click', UI.goToNext);
-        DOM.titleContainer.addEventListener('click', UI.refreshScores);
+        // DOM.titleContainer.addEventListener('click', UI.refreshScores);
         DOM.refreshGameBtn.addEventListener('click', UI.refreshScores);
         DOM.refreshStudentsBtn.addEventListener('click', UI.refreshStudents);
         DOM.shuffleStudentsBtn.addEventListener('click', UI.shufflePreview);
